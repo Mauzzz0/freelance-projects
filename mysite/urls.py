36cd9b60e2d54d80582e67f46e2c068d446abe39
Mauzzz0 <ruslan.semack1501@gmail.com>
2021-01-04 21:29:52 +0300

    del urls and .html for /schedule and /news

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,7 +5,5 @@ from django.conf.urls import url
 urlpatterns = [
     url('^admin/', admin.site.urls),
     url('', include('mainApp.urls')),
-    url('^webexample/', include('webexample.urls')),
-    url('^schedule/', include('schedule.urls')),
     url('^news/', include('news.urls')),
 ]
