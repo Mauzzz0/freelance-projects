958f2c396a3926cf0cb68bdd307764f1ad9bb327
Mauzzz0 <ruslan.semack1501@gmail.com>
2021-01-04 21:22:49 +0300

    del /schedule and /webexmaple

--- schedule/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def index(request):
-    data = {
-        "tittle" : "переданный текст из словаря внутри метода index() в views.py",
-        "values": ["text1","text2","text3"]
-    }
-    return render(request, 'mainApp/schedule.html', data)
--- webexample/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-# Create your views here.
-
-def index(request):
-    return HttpResponse("<h3>Хэдер3. Хардкод из .py (webexample/views.py)</h3>")
