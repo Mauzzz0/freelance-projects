0041eda95461ec613d31411ae5379025b8e13666
Mauzzz0 <ruslan.semack1501@gmail.com>
2021-02-20 04:23:08 +0300

    small cleaning

--- rthl_site/urls.py
+++ rthl_site/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
-    path('', views.Home, name="home"), #TODO: Choose homepage
+    path('', views.Home, name="home"),
     path('schedule/',views.schedule, name="schedule"),
     path('news/',views.news, name="news"),
     path('teams/',views.teams, name="teams"),
@@ -11,13 +10,11 @@ urlpatterns = [
     path('insurance/',views.insurance, name="insurance"),
     path('contacts/',views.contacts, name="contacts"),
 
-    path('dev_create_app', views.create_app, name="create_app"),
     path('dev_news', views.news, name="news"),
     path('dev_scoreboard', views.scoreboard, name="scoreboard"),
-    path('match/<int:pk>', views.MatchDetailView.as_view()),
 
+    path('match/<int:pk>', views.MatchDetailView.as_view()),
     path('team/<slug:slug>', views.TeamDetailView.as_view()),
     path('team/<slug:slug>/create_app', views.TeamAppDetailView.as_view()),
     path('player/<int:pk>', views.PlayerDetailView.as_view()),
-
 ]
