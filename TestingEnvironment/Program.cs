702ce322d497c45c37fc1ecb684831e7bb18cef2
Mauzzz0 <ruslan.semack1501@gmail.com>
2021-04-17 13:28:16 +0300

    pause

--- TestingEnvironment/Program.cs
+++ TestingEnvironment/Program.cs
@@ -5,36 +5,68 @@ namespace TestingEnvironment
 {
     class Program
     {
+        private static DateTime start;
+        private static DateTime stopDissolution;
+        private static DateTime restartDissolution;
+        private static int penaltyScores = 0;
         static void Main(string[] args)
         {
-            
             while (true)
             {
-                WriteLine("1 - подписаться\n2-вызвать событие");
-                string inp = Console.ReadLine();
+                WriteLine("\n1 - подписаться\n2 - вызвать нештатную ситуацию\n3 - вызвать зелёный семафор");
+                string inp = ReadLine();
                 if (inp == "1")
                 {
                     CriticalSituationHappened += c_CriticalSituationHappened;
+                    StateSemaphoreHappened += c_StateSemaphoreHappened;
                 }
                 else if (inp == "2")
                 {
                     Program M = new Program();
-                    M.EventCall();
+                    M.CriticalSituationBrokenGACCall();
+                }
+                else if (inp == "3")
+                {
+                    Program M = new Program();
+                    M.StateSemaphoreRedCall();
                 }
             }
 
             static void c_CriticalSituationHappened(object sender, CriticalSituationGacEventArgs e)
             {
-                Console.WriteLine("Событие произошло");
+                start = DateTime.Now;
+                WriteLine("Нештатная ситуация. "+e.TypeDisrepair+" "+Convert.ToString(start.TimeOfDay));
+            }
+
+            static void c_StateSemaphoreHappened(object sender, StateSemaphoreEventArgs e)
+            {
+                stopDissolution = DateTime.Now;
+                double difference = (stopDissolution.TimeOfDay - start.TimeOfDay).TotalSeconds;
+                penaltyScores = difference < 15 ? 0 : Convert.ToInt32(Math.Floor(100 * (difference - 5)));
+                WriteLine("Остановка роспуска. " + stopDissolution.TimeOfDay);
+                WriteLine("Время между событиями: " + difference);
+                WriteLine("Штрафные баллы: " + penaltyScores);
+                // TODO: Чекнуть BrakeControlMode
             }
         }
 
-        void EventCall()
+        void CriticalSituationBrokenGACCall()
         {
             CriticalSituationGacEventArgs args = new CriticalSituationGacEventArgs();
             args.Message = "НЕШТАТНАЯ СИТУАЦИЯ: ГАЦ МН НЕИСПРАВЕН!";
             args.TypeDisrepair = TypeDisrepairGac.BrokenGAC;
             OnCriticalSituationHappened(args);
+        }
+        void StateSemaphoreRedCall()
+        {
+            StateSemaphoreEventArgs args = new StateSemaphoreEventArgs();
+            args.Owner = new Role();
+            args.Speed = 10.15;
+            args.ButtonStage = new StageSemaphore();
+            args.IdObj = new Guid();
+            args.IdSemaphore = new Guid();
+            args.ValueColor = SemaphoreColor.Red;
+            OnStateSemaphoreHappened(args);
         }
 
         protected virtual void OnCriticalSituationHappened(CriticalSituationGacEventArgs e)
@@ -45,7 +77,16 @@ namespace TestingEnvironment
                 handler(this, e);
             }
         }
+        protected virtual void OnStateSemaphoreHappened(StateSemaphoreEventArgs e)
+        {
+            EventHandler<StateSemaphoreEventArgs> handler = StateSemaphoreHappened;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
 
         public static event EventHandler<CriticalSituationGacEventArgs> CriticalSituationHappened;
+        public static event EventHandler<StateSemaphoreEventArgs> StateSemaphoreHappened;
     }
 }
