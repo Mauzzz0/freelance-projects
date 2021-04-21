5a3121b7fb5a687256bfcd662d0d0b0c9a036409
Mauzzz0 <ruslan.semack1501@gmail.com>
2021-04-21 18:26:13 +0300

    Первое решено

--- task1.py
+++ task1.py
@@ -2,10 +2,13 @@ def morning(*kwargs):
     words = [el for el in kwargs]
     output = dict()
     for word in words:
+        old_letter = ""
         for letter in word:
-            if letter.lower() in ["a", "e", "i", "o", "u", "y"]:
+
+            if letter.lower() in ["a", "e", "i", "o", "u", "y"] and letter.lower() not in old_letter:
                 letter = letter.lower()
                 output[letter] = output.get(letter, 0) + 1
+                old_letter += letter
     return output
 
 data = ["The", "sky", "was", "blue", "and", "shining"]
