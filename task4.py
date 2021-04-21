157ea412f1d3a040bc5e484a0266bb8decfe9294
Mauzzz0 <ruslan.semack1501@gmail.com>
2021-04-21 19:41:05 +0300

    4

--- task4.py
+++ task4.py
@@ -8,15 +8,16 @@ dig = 0
 arr = list()
 out = list()
 for line in sys.stdin:
+    out = list()
     if c == 0:
         dig = int(line)
-    elif c == 1:
+    else:
         arr = line.split(": ")
-        break
-    c += 1
-arr[-1] = arr[-1].replace("\n", "")
-for word in arr:
-    if len(word) % dig != 0:
-        out.append(word)
+        arr[-1] = arr[-1].replace("\n", "")
+        for word in arr:
+            if len(word) % dig != 0:
+                out.append(word)
+        print(*out, sep=", ")
 
-print(*out,sep=", ")
+    c += 1
+
