8651b05dc66705997a170cf5933037e02ff01acb
Mauzzz0 <ruslan.semack1501@gmail.com>
2021-05-16 18:03:26 +0300

    beta2

--- TestingEnvironmentSwitches/CorrectBehaviorWhenSwitchCriticalSituation.cs
+++ TestingEnvironmentSwitches/CorrectBehaviorWhenSwitchCriticalSituation.cs
@@ -8,6 +8,7 @@ namespace TestingEnvironmentSwitches
 {
     public class CorrectBehaviorWhenSwitchCriticalSituation
     {
+        public bool isStarted { get; private set; } // началась ли обработка события
         public int standartStopDissolutionTime { get; private set; } // время на остановку роспуска
         public int standartChangeSwitchesTime { get; private set; } // время на изменение маршрутов
         public int standartRestartDissolutionTime { get; private set; }
@@ -18,7 +19,7 @@ namespace TestingEnvironmentSwitches
         public int penaltyScores { get; private set; } // начисленные штрафные очки
         public int penaltyMultiplicator { get; private set; } // множитель штрафа. по умолчания = 100
         private IEnumerable<string> originWays;
-        private IEnumerable<string> newWays;
+        private IEnumerable<string> otcepWays;
 
         public CorrectBehaviorWhenSwitchCriticalSituation(int timeForChangeSwitches, int timeForDissolutionStop = 5, int multiplicator = 100, int timeForDissolutionRestart = 5)
         {
@@ -27,75 +28,115 @@ namespace TestingEnvironmentSwitches
             penaltyMultiplicator = multiplicator;
             standartRestartDissolutionTime = timeForDissolutionRestart;
         }
+
+        public void reset()
+        {
+            penaltyScores = 0;
+            isStarted = false;
+            criticalSituationStartTime = DateTime.MinValue;
+            stopDissolutionTime = DateTime.MinValue;
+            restartDissolutionTime = DateTime.MinValue;
+            previousColor = SemaphoreColor.Green;
+        }
         public void CriticalSituationSwitchHappenedHandler(object sender, CriticalSituationSwitchEventArgs e)
         { // Получаем сообщение о нештатной ситуации, определяем пути, которые следуют за данной стрелкой.
-            criticalSituationStartTime = DateTime.Now;
-            Console.WriteLine("Ситуация сработала " + criticalSituationStartTime);
-            Console.WriteLine("Запрос путей");
-            // Передали id стрелки в запрос
-            // Получили временно статичный ответ
-            originWays = Map.GetWaysForSwich(e.IdObj);
-            Console.WriteLine("Получены пути:");
-            foreach (string var in originWays)
+            if (!isStarted)
             {
-                Console.WriteLine(var);
+                isStarted = true;
+                criticalSituationStartTime = DateTime.Now;
+                Console.WriteLine("Ситуация сработала " + criticalSituationStartTime);
+                // Передали id стрелки в запрос
+                // Получили временно статичный ответ
+                originWays = Map.GetWaysForSwich(e.IdObj);
+            }
+            else if (isStarted & e.TypeDisrepair == TypeDisrepairSwitch.None)
+            {
+                isStarted = false;
+                criticalSituationStartTime = DateTime.MinValue;
+                stopDissolutionTime = DateTime.MinValue;
+                restartDissolutionTime = DateTime.MinValue;
+                previousColor = SemaphoreColor.Green;
             }
         }
 
         public void SemaphoreChangeHappenedHandler(object sender, StateSemaphoreEventArgs e)
         {
-            Console.WriteLine("Сработало изменение семафора");
-            if (e.ValueColor == SemaphoreColor.Red)
+            if (isStarted)
             {
-                stopDissolutionTime = DateTime.Now;
-                Console.WriteLine("Красный сигнал. Роспуск остановлен спустя " + (stopDissolutionTime - criticalSituationStartTime).TotalSeconds);
-                if ((stopDissolutionTime - criticalSituationStartTime).TotalSeconds > standartStopDissolutionTime)
+                Console.WriteLine("Сработало изменение семафора");
+                if (e.ValueColor == SemaphoreColor.Red)
                 {
-                    int penalty = Convert.ToInt32((stopDissolutionTime - criticalSituationStartTime).TotalSeconds -
-                                                  standartStopDissolutionTime) * penaltyMultiplicator;
-                    penaltyScores += penalty;
-                }
-                previousColor = e.ValueColor;
-                Console.WriteLine("Всего штрафных баллов: " + penaltyScores);
-            }
-            else
-            {
-                if (previousColor == SemaphoreColor.Red)
-                {
-                    restartDissolutionTime = DateTime.Now;
-                    Console.WriteLine("Рестарт роспуска спустя " +
-                                      (restartDissolutionTime - stopDissolutionTime).TotalSeconds);
-                    if ((restartDissolutionTime - stopDissolutionTime).TotalSeconds > standartRestartDissolutionTime)
+                    stopDissolutionTime = DateTime.Now;
+                    Console.WriteLine("Красный сигнал. Роспуск остановлен спустя " +
+                                      (stopDissolutionTime - criticalSituationStartTime).TotalSeconds);
+                    if ((stopDissolutionTime - criticalSituationStartTime).TotalSeconds > standartStopDissolutionTime)
                     {
-                        int penalty = Convert.ToInt32((restartDissolutionTime - stopDissolutionTime).TotalSeconds -
-                                                      standartRestartDissolutionTime) * penaltyMultiplicator;
+                        int penalty = Convert.ToInt32((stopDissolutionTime - criticalSituationStartTime).TotalSeconds -
+                                                      standartStopDissolutionTime) * penaltyMultiplicator;
                         penaltyScores += penalty;
                     }
+
                     previousColor = e.ValueColor;
                     Console.WriteLine("Всего штрафных баллов: " + penaltyScores);
-                    // Рестар произошёл, отправляется сообщение с запросом маршрута
-                    Console.WriteLine("Запрос путей");
-                    // Передали id стрелки в запрос
-                    // Получили временно статичный ответ
-                    newWays = Map.GetWaysForSwich(e.IdObj); // Обращаемся не к ID семафора, а к ID пути надвига
-                    Console.WriteLine("Получены пути:");
-                    foreach (string var in newWays)
+                }
+                else
+                {
+                    if (previousColor == SemaphoreColor.Red)
                     {
-                        Console.WriteLine(var);
-                    }
-
-                    foreach (string way in newWays)
-                    {
-                        if (originWays.Contains(way))
+                        restartDissolutionTime = DateTime.Now;
+                        Console.WriteLine("Рестарт роспуска спустя " +
+                                          (restartDissolutionTime - stopDissolutionTime).TotalSeconds);
+                        if ((restartDissolutionTime - stopDissolutionTime).TotalSeconds >
+                            standartRestartDissolutionTime)
                         {
-                            Console.WriteLine("ОДИНАКОВЫЙ ПУТЬ: " + way);
-                            penaltyScores += penaltyMultiplicator;
+                            int penalty = Convert.ToInt32((restartDissolutionTime - stopDissolutionTime).TotalSeconds -
+                                                          standartRestartDissolutionTime) * penaltyMultiplicator;
+                            penaltyScores += penalty;
                         }
-                        
+
+                        previousColor = e.ValueColor;
+                        Console.WriteLine("Всего штрафных баллов: " + penaltyScores);
+                        // Рестар произошёл, отправляется сообщение с запросом маршрута
+                        Console.WriteLine("Запрос путей");
+                        // TODO: Вызов GetObjectStatesEventArgs
+                        // Здесь должен быть вызов GetObjectStatesEventArgs, в ответ на который
+                        // произойдёт SortListEventArgs
+                        // TODO: Вызов GetObjectStatesEventArgs
+
+                        //foreach (string var in newWays)
+                        //{
+                        //    Console.WriteLine(var);
+                        //}
+
+                        //foreach (string way in newWays)
+                        //{
+                         //   if (originWays.Contains(way))
+                          //  {
+                            //    Console.WriteLine("ОДИНАКОВЫЙ ПУТЬ: " + way);
+                              //  penaltyScores += penaltyMultiplicator;
+                            //}
+
+                        //}
+
+                        Console.WriteLine("Всего штрафных баллов: " + penaltyScores);
                     }
-                    Console.WriteLine("Всего штрафных баллов: " + penaltyScores);
                 }
             }
+        }
+
+        public void SortListHappenedHandler(object sender, SortListEventArgs e)
+        {
+            int incorrectWays = 0;
+            foreach (OtcepKsau ok in e.CollectionSortList[0].ListOtcep)
+            {
+                if (originWays.Contains(ok.Route.IdWay))
+                {
+                    incorrectWays++;
+                }
+            }
+            Console.WriteLine("Кол-во неперевелённых путей: " + incorrectWays);
+            penaltyScores += incorrectWays * penaltyMultiplicator;
+            Console.WriteLine("Всего штрафных баллов: " + penaltyScores);
         }
     }
 
@@ -146,15 +187,10 @@ namespace TestingEnvironmentSwitches
     class Map
     {
         public static IEnumerable<string> GetWaysForSwich(Guid idSwitch)
-        {
-            if (DateTime.Now.Second % 2 == 0)
-            {
-                return new List<string>{"aa","bb", "cc"};
-            }
-            return new List<string>{"gg","gg", "gg"};
+        { // TODO:
+            return new List<string>{"aa","bb", "cc"};
         }
     }
-    //public static IEnumerable<string> GetWaysForSwich(Guid idSwitch)
     
 
     public class GetObjectStatesEventArgs : EventArgs
@@ -178,13 +214,13 @@ namespace TestingEnvironmentSwitches
 
     public class SortList
     {
-        public List<OtcepKsau> ListOtcep { get; private set; }
+        public List<OtcepKsau> ListOtcep { get; set; } // временный паблик сет
     }
 
     public class OtcepKsau
     {
-        string _route; // Догадка
-        public string Route
+        Route _route; // Догадка
+        public Route Route
         {
             get { return _route; }
             set { _route = value; }
--- TestingEnvironmentSwitches/Program.cs
+++ TestingEnvironmentSwitches/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Moq;
 
 namespace TestingEnvironmentSwitches
 {
@@ -7,50 +10,227 @@ namespace TestingEnvironmentSwitches
         private static Program M;
         static void Main(string[] args)
         {
-            while (true)
+            Console.WriteLine("1 - ручное логирование, 2 - автотесты");
+            string inp = Console.ReadLine();
+            if (inp == "1")
             {
-                Console.WriteLine("\n1 - подписаться\n2 - CriticalSituationSwitchCall\n3 - Остановить роспуск\n4 - Рестарт роспуска\n5 - \n11 - \n12 - \n13 - ");
-                string inp = Console.ReadLine();
-                if (inp == "1")
+                while (true)
                 {
-                    M = new Program();
-                    CorrectBehaviorWhenSwitchCriticalSituation a = new CorrectBehaviorWhenSwitchCriticalSituation(10);
-                    CriticalSituationSwitchHappened += a.CriticalSituationSwitchHappenedHandler;
-                    ChangeSemaphoreHappened += a.SemaphoreChangeHappenedHandler;
-                }
-                else if (inp == "2")
-                {
-                    M.CriticalSituationSwitchCall();
-                }
-                else if (inp == "3")
-                {
-                    M.DissolutionStopCall();
-                }
-                else if (inp == "4")
-                {
-                    M.DissolutionRestartCall();
-                }
-                else if (inp == "5")
-                {
-                    
-                }
-                else if (inp == "11")
-                {
-                    
-                }
-                else if (inp == "12")
-                {
-                    
-                }
-                else if (inp == "13")
-                {
-                    
+                    Console.WriteLine(
+                        "\n1 - подписаться\n2 - CriticalSituationSwitchCall\n3 - Остановить роспуск\n4 - Рестарт роспуска\n5 - GetObjectStates");
+                    inp = Console.ReadLine();
+                    if (inp == "1")
+                    {
+                        M = new Program();
+                        CorrectBehaviorWhenSwitchCriticalSituation a =
+                            new CorrectBehaviorWhenSwitchCriticalSituation(10);
+                        CriticalSituationSwitchHappened += a.CriticalSituationSwitchHappenedHandler;
+                        ChangeSemaphoreHappened += a.SemaphoreChangeHappenedHandler;
+                        SortListHappened += a.SortListHappenedHandler;
+                    }
+                    else if (inp == "2")
+                    {
+                        M.CriticalSituationSwitchCall();
+                    }
+                    else if (inp == "3")
+                    {
+                        M.DissolutionStopCall();
+                    }
+                    else if (inp == "4")
+                    {
+                        M.DissolutionRestartCall();
+                    }
+                    else if (inp == "5")
+                    {
+                        M.GetObjectStatesCall(1);
+                    }
                 }
             }
-            
-            
+            else
+            {
+                Console.WriteLine("====Тесты запущены=====\n" +
+                                  "Время на изменение стрелок: 5сек\n" +
+                                  "Время на остановку роспуска: 5сек\n" +
+                                  "Время на рестарт роспуска: 5сек\n" +
+                                  "Множитель штрафа: 100\n" +
+                                  "========================\n\n");
+                
+                Console.WriteLine("====1 кейс====\n" +
+                                  "Нет опозданий, все стрелки переведены\n" +
+                                  "Должно получиться 0 баллов.");
+                M = new Program();
+                CorrectBehaviorWhenSwitchCriticalSituation a =
+                    new CorrectBehaviorWhenSwitchCriticalSituation(10);
+                CriticalSituationSwitchHappened += a.CriticalSituationSwitchHappenedHandler;
+                ChangeSemaphoreHappened += a.SemaphoreChangeHappenedHandler;
+                SortListHappened += a.SortListHappenedHandler;
+                M.CriticalSituationSwitchCall();
+                M.DissolutionStopCall();
+                M.DissolutionRestartCall();
+                M.GetObjectStatesCall(0);
+                Console.WriteLine("Получилось: "+a.penaltyScores);
+                if (a.penaltyScores == 0)
+                {
+                    Console.WriteLine("====Тест пройден====\n\n");
+                }
+                else
+                {
+                    Console.WriteLine("==Тест не пройден==\n\n");
+                }
+                
+                Console.WriteLine("====2 кейс====\n" +
+                                  "Нет опозданий, 3 стрелки не переведены\n" +
+                                  "Должно получиться 300 баллов.");
+                a.reset();
+                M.CriticalSituationSwitchCall();
+                M.DissolutionStopCall();
+                M.DissolutionRestartCall();
+                M.GetObjectStatesCall(1);
+                Console.WriteLine("Получилось: "+a.penaltyScores);
+                if (a.penaltyScores == 300)
+                {
+                    Console.WriteLine("====Тест пройден====\n\n");
+                }
+                else
+                {
+                    Console.WriteLine("==Тест не пройден==\n\n");
+                }
+                
+                Console.WriteLine("====3 кейс====\n" +
+                                  "Опоздание роспуска на 1сек, все стрелки переведены\n" +
+                                  "Должно получиться 100 баллов.");
+                a.reset();
+                M.CriticalSituationSwitchCall();
+                Thread.Sleep(6000);
+                M.DissolutionStopCall();
+                M.DissolutionRestartCall();
+                M.GetObjectStatesCall(0);
+                Console.WriteLine("Получилось: "+a.penaltyScores);
+                if (a.penaltyScores == 100)
+                {
+                    Console.WriteLine("====Тест пройден====\n\n");
+                }
+                else
+                {
+                    Console.WriteLine("==Тест не пройден==\n\n");
+                }
+                
+                Console.WriteLine("====4 кейс====\n" +
+                                  "Опоздание роспуска на 1сек, 3 стрелки не переведены\n" +
+                                  "Должно получиться 400 баллов.");
+                a.reset();
+                M.CriticalSituationSwitchCall();
+                Thread.Sleep(6000);
+                M.DissolutionStopCall();
+                M.DissolutionRestartCall();
+                M.GetObjectStatesCall(1);
+                Console.WriteLine("Получилось: "+a.penaltyScores);
+                if (a.penaltyScores == 400)
+                {
+                    Console.WriteLine("====Тест пройден====\n\n");
+                }
+                else
+                {
+                    Console.WriteLine("==Тест не пройден==\n\n");
+                }
+                
+                Console.WriteLine("====5 кейс====\n" +
+                                  "Опоздание рестарта на 1сек, все стрелки переведены\n" +
+                                  "Должно получиться 100 баллов.");
+                a.reset();
+                M.CriticalSituationSwitchCall();
+                M.DissolutionStopCall();
+                Thread.Sleep(1000+ a.standartRestartDissolutionTime*1000);
+                M.DissolutionRestartCall();
+                M.GetObjectStatesCall(1);
+                Console.WriteLine("Получилось: "+a.penaltyScores);
+                if (a.penaltyScores == 100)
+                {
+                    Console.WriteLine("====Тест пройден====\n\n");
+                }
+                else
+                {
+                    Console.WriteLine("==Тест не пройден==\n\n");
+                }
+                
+                Console.WriteLine("====6 кейс====\n" +
+                                  "Опоздание рестарта на 1сек, 3 стрелки не переведены\n" +
+                                  "Должно получиться 400 баллов.");
+                a.reset();
+                M.CriticalSituationSwitchCall();
+                M.DissolutionStopCall();
+                Thread.Sleep(1000+ a.standartRestartDissolutionTime*1000);
+                M.DissolutionRestartCall();
+                M.GetObjectStatesCall(1);
+                Console.WriteLine("Получилось: "+a.penaltyScores);
+                if (a.penaltyScores == 400)
+                {
+                    Console.WriteLine("====Тест пройден====\n\n");
+                }
+                else
+                {
+                    Console.WriteLine("==Тест не пройден==\n\n");
+                }
+            }
         }
 
+        void GetObjectStatesCall(int id)
+        {
+            if (id == 1)
+            {
+                SortListEventArgs args = new SortListEventArgs();
+                args.CollectionSortList = new List<SortList>() {new SortList()};
+                args.CollectionSortList[0].ListOtcep = new List<OtcepKsau>();
+
+                Route r1 = new Route();
+                r1.IdWay = "aa";
+                OtcepKsau ok1 = new OtcepKsau();
+                ok1.Route = r1;
+
+                Route r2 = new Route();
+                r2.IdWay = "bb";
+                OtcepKsau ok2 = new OtcepKsau();
+                ok2.Route = r2;
+
+                Route r3 = new Route();
+                r3.IdWay = "cc";
+                OtcepKsau ok3 = new OtcepKsau();
+                ok3.Route = r3;
+
+
+                args.CollectionSortList[0].ListOtcep.Add(ok1);
+                args.CollectionSortList[0].ListOtcep.Add(ok2);
+                args.CollectionSortList[0].ListOtcep.Add(ok3);
+                OnSortListHappened(args);
+            }
+            else
+            {
+                SortListEventArgs args = new SortListEventArgs();
+                args.CollectionSortList = new List<SortList>() {new SortList()};
+                args.CollectionSortList[0].ListOtcep = new List<OtcepKsau>();
+
+                Route r1 = new Route();
+                r1.IdWay = "dd";
+                OtcepKsau ok1 = new OtcepKsau();
+                ok1.Route = r1;
+
+                Route r2 = new Route();
+                r2.IdWay = "ee";
+                OtcepKsau ok2 = new OtcepKsau();
+                ok2.Route = r2;
+
+                Route r3 = new Route();
+                r3.IdWay = "ff";
+                OtcepKsau ok3 = new OtcepKsau();
+                ok3.Route = r3;
+
+
+                args.CollectionSortList[0].ListOtcep.Add(ok1);
+                args.CollectionSortList[0].ListOtcep.Add(ok2);
+                args.CollectionSortList[0].ListOtcep.Add(ok3);
+                OnSortListHappened(args);
+            }
+        }
         void DissolutionRestartCall()
         {
             StateSemaphoreEventArgs args = new StateSemaphoreEventArgs();
@@ -89,7 +269,27 @@ namespace TestingEnvironmentSwitches
                 handler(this, e);
             }
         }
+
+        protected virtual void OnSortListHappened(SortListEventArgs e)
+        {
+            EventHandler<SortListEventArgs> handler = SortListHappened;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+
+        protected virtual void OnGetObjectStatesHappened(GetObjectStatesEventArgs e)
+        {
+            EventHandler<GetObjectStatesEventArgs> handler = GetObjectStatesHappened;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
         public static event EventHandler<CriticalSituationSwitchEventArgs> CriticalSituationSwitchHappened;
         public static event EventHandler<StateSemaphoreEventArgs> ChangeSemaphoreHappened;
+        public static event EventHandler<SortListEventArgs> SortListHappened;
+        public static event EventHandler<GetObjectStatesEventArgs> GetObjectStatesHappened;
     }
 }
